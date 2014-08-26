3ff8d3f8920c0b1c796254fcc06b0062268e5f9d
Wojciech Bożek <wojciech.bozek@ironin.pl>
2014-08-26 21:54:29 +0200

    Fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post }
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
